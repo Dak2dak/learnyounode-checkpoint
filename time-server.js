3e0aa10fbfcf550412968b61b47a480f6cf976a0
Didier Dakoury <dazibao76@gmail.com>
2022-03-22 15:34:08 +0000

    adding tenth pb's solution

---
+++ time-server.js
@@ -0,0 +1,21 @@
+const net = require('net');
+const array = process.argv;
+
+function zeroFill(i) {
+    return (i < 10 ? '0' : '') + i;
+}
+
+function now() {
+    let d = new Date();
+    return d.getFullYear() + '-'
+        + zeroFill(d.getMonth() + 1) + '-'
+        + zeroFill(d.getDate()) + ' '
+        + zeroFill(d.getHours()) + ':'
+        + zeroFill(d.getMinutes());
+}
+
+let server = net.createServer(function (socket) {
+    socket.end(now() + '\n');
+});
+
+server.listen(Number(array[2]));
